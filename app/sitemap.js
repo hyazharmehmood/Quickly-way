853cbf26e801ac427b15a142cc584fc0a8112b9d
hyazharmehmood <hyazharmehmood@gmail.com>
2026-01-16 15:55:09 +0500

    refactor: Comment out become-seller URL entry in sitemap to streamline sitemap generation and remove unused routes.

--- app/sitemap.js
+++ app/sitemap.js
@@ -20,11 +20,11 @@ export default function sitemap() {
             changeFrequency: 'monthly',
             priority: 0.8,
         },
-        {
-            url: `${baseUrl}/become-seller`,
-            lastModified: new Date(),
-            changeFrequency: 'weekly',
-            priority: 0.9,
-        },
+        // {
+        //     url: `${baseUrl}/become-seller`,
+        //     lastModified: new Date(),
+        //     changeFrequency: 'weekly',
+        //     priority: 0.9,
+        // },
     ];
 }
